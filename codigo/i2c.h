e2ca13d91eb4339190302fb6bbdeeba53b5d44e9
Patricio Iribarne Catella <patricio_14_94@hotmail.com>
2019-10-30 15:01:28 -0300

    Add start function for I2C

--- codigo/i2c.h
+++ codigo/i2c.h
@@ -1,6 +1,21 @@
 #ifndef I2C_H
 #define I2C_H
 
+// Mask STATUS CODE
+#define STAT_CODE 0xF8
+
+// Start code
+#define START 0x08
+
+// Slave Address in HIGH mode
+#define SLA 0x69
+// Slave Address + Master mode: Read
+// SLA_R
+#define SLA_R (SLA + 0)
+
+// SLA_R_ACK code
+#define SLA_R_ACK 0x40
+
 // TWCR bits
 #define TWINT 7
 #define TWEA 6
