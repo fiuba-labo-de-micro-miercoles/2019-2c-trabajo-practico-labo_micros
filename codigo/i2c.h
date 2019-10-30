119ea07875b735cac5abc340ce1fb847bd5ae793
PatricioIribarneCatella <patricio_14_94@hotmail.com>
2019-10-30 01:27:04 -0300

    Add more functions

--- codigo/i2c.h
+++ codigo/i2c.h
@@ -1,5 +1,17 @@
 #ifndef I2C_H
 #define I2C_H
 
+// TWCR bits
+#define TWINT 7
+#define TWEA 6
+#define TWSTA 5
+#define TWSTO 4
+#define TWWC 3
+#define TWEN 2
+#define TWIE 0
+
+// TWSR bits
+#define TWPS0 0
+#define TWPS1 1
 
 #endif // I2C_H
