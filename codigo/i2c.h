37effa7fb5989a3cbf0441004334a3da76d07984
PatricioIribarneCatella <patricio_14_94@hotmail.com>
2019-10-31 16:17:18 -0300

    Add I2C protocol: divided into different functions to START, CONNECT, READ and END the connection

--- codigo/i2c.h
+++ codigo/i2c.h
@@ -1,26 +1,65 @@
 #ifndef I2C_H
 #define I2C_H
 
-// Mask STATUS CODE
-#define STAT_CODE 0xF8
+// Default MPU 6050 address
+#define MPU_6050_DEF_ADDR 0x68
 
+// Mask STATUS CODE
+#define STAT_CODE_MASK 0xF8
+
+///////////////////////////////////
+//      General status code      //
+///////////////////////////////////
+//
 // Start code
 #define START 0x08
+// Repeated Start
+#define REP_START 0x10
 
-// SLA_R_ACK code
+/////////////////////////////////////////
+//    Master Transmitter status code   //
+/////////////////////////////////////////
+// 
+// SLA+W transmitted and ACK received
+#define SLA_W_ACK 0x18
+// SLA+W transmitted and NACK received
+#define SLA_W_NACK 0x20
+// Data byte transmitted and ACK received
+#define DATA_SEND_ACK 0x28
+// Data byte transmitted and NACK received
+#define DATA_SEND_NACK 0x30
+
+/////////////////////////////////////////
+//     Master Receiver status code     //
+/////////////////////////////////////////
+// 
+// SLA+R transmitted and ACK received
 #define SLA_R_ACK 0x40
-
-// Data byte received and
-// ACK send
+// SLA+R transmitted and NACK received
+#define SLA_R_NACK 0x48
+// Data byte received and ACK returned
 #define DATA_RECV_ACK 0x50
+// Data byte returned and NACK returned
+#define DATA_RECV_NACK 0x58
 
+
+////////////////////////
+//    Slave Address   //
+////////////////////////
+//
 // Slave Address in HIGH mode
-#define SLA 0x69
+#define SLA_H 0x69
+// Slave Address + Master mode: write
+// SLA_W
+#define SLA_W (SLA_H<<1 + 0)
 // Slave Address + Master mode: Read
 // SLA_R
-#define SLA_R (SLA + 0)
+#define SLA_R (SLA_H<<1 + 1)
 
-
+/////////////////////
+//  Register bits  //
+/////////////////////
+//
 // TWCR bits
 #define TWINT 7
 #define TWEA 6
