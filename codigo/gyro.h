985f6a067d640b353a118753aacccba56d05207e
Patricio Iribarne Catella <patricio_14_94@hotmail.com>
2019-11-02 22:13:02 -0300

    Some changes

--- codigo/gyro.h
+++ codigo/gyro.h
@@ -18,22 +18,22 @@
 //      Acceleration Registers     //
 /////////////////////////////////////
 //
-#define ACCEL_X_H r17
-#define ACCEL_X_L r18
-#define ACCEL_Y_H r19
-#define ACCEL_Y_L r20
-#define ACCEL_Z_H r21
-#define ACCEL_Z_L r22
+#define ACCEL_X_H r18
+#define ACCEL_X_L r19
+#define ACCEL_Y_H r20
+#define ACCEL_Y_L r21
+#define ACCEL_Z_H r22
+#define ACCEL_Z_L r23
 
 //////////////////////////////////
 //      Gyroscope Registers     //
 //////////////////////////////////
 //
-#define GYRO_X_H r17
-#define GYRO_X_L r18
-#define GYRO_Y_H r19
-#define GYRO_Y_L r20
-#define GYRO_Z_H r21
-#define GYRO_Z_L r22
+#define GYRO_X_H r18
+#define GYRO_X_L r19
+#define GYRO_Y_H r20
+#define GYRO_Y_L r21
+#define GYRO_Z_H r22
+#define GYRO_Z_L r23
 
 #endif // GYRO_H
