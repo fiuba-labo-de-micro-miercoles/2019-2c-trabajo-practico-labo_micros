46f2f0021f2fc62dc5361783f53669c0e9c264cf
PatricioIribarneCatella <patricio_14_94@hotmail.com>
2019-11-16 13:29:38 -0300

    WIP: more changes in C test

--- codigo/stepper/test_stepper.c
+++ codigo/stepper/test_stepper.c
@@ -7,8 +7,8 @@
 
 #include "stepper.h"
 
-#define MAX 4
-#define MAX_ITER 255
+#define MAX 8
+#define MAX_ITER 256
 
 #define CW_MODE 0x1
 #define CCW_MODE 0x0
@@ -18,11 +18,22 @@
 #define PIN_2 5
 #define PIN_3 6
 
-unsigned const char rotation[MAX] PROGMEM = {0x08, 0x10, 0x20, 0x04};
-static unsigned char STEP_NUM;
+unsigned const char rotation[MAX] PROGMEM = {0x08, 0x18, 0x10, 0x30, 0x20, 0x60, 0x40, 0x48};
+unsigned char STEP_NUM;
 
 void setup(void) {
+
+	// config digital pins
+	// mapping:
+	//  - IN1 -> pin 9: PORTH[6]
+	//  - IN2 -> pin 8: PORTH[5]
+	//  - IN3 -> pin 7: PORTH[4]
+	//  - IN4 -> pin 6: PORTH[3]
+	// in output mode
+
 	DDRH = (1<<PIN_0)|(1<<PIN_1)|(1<<PIN_2)|(1<<PIN_3);
+
+	STEP_NUM = 0;
 }
 
 void one_step(unsigned char mode) {
@@ -33,7 +44,7 @@ void one_step(unsigned char mode) {
 	step = STEP_NUM;
 
 	if (!mode)
-		idx = 3 - step;
+		idx = 7 - step;
 	else
 		idx = step;
 
@@ -43,7 +54,7 @@ void one_step(unsigned char mode) {
 
 	step++;
 
-	if (step > 4)
+	if (step == MAX)
 		step = 0;
 
 	STEP_NUM = step;
@@ -53,15 +64,29 @@ void move(unsigned char mode) {
 
 	for (int i = 0; i < MAX_ITER; i++) {
 		one_step(mode);
-		_delay_ms(2);
+		_delay_ms(1);
 	}
 }
 
 void loop(void) {
 
 	move(CW_MODE);
+	move(CW_MODE);
+	move(CW_MODE);
+	move(CW_MODE);
+	move(CW_MODE);
+	move(CW_MODE);
+	move(CW_MODE);
+	move(CW_MODE);
 	_delay_ms(1000);
 
+	move(CCW_MODE);
+	move(CCW_MODE);
+	move(CCW_MODE);
+	move(CCW_MODE);
+	move(CCW_MODE);
+	move(CCW_MODE);
+	move(CCW_MODE);
 	move(CCW_MODE);
 	_delay_ms(1000);
 }
